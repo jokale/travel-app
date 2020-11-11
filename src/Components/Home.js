6a1e73ef47546c9ea723424b67663dd5b8b02291
jokale <kalemajoanna@gmail.com>
2020-11-11 20:42:01 +0000

    add orange background

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,10 @@
-import { render } from '@testing-library/react'
 import React from 'react'
 
 
 class Home extends React.Component{
     render(){
         return(
-            <div>
+            <div id='home'>
                 <h1>Welcome to Travels</h1>
                 <button><a href='https://www.tripadvisor.co.uk/' target="_blank" rel="noopener noreferrer"></a></button>
             </div>
